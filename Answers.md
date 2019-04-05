95288f32d4d53a1ec7504e870282094ea9f9d28b
Brandon_J_Allison <you@example.com>
2019-04-05 13:18:00 -0400

    questions answered

--- Answers.md
+++ Answers.md
@@ -1,5 +1,19 @@
 1.  Explain the differences between `client-side routing` and `server-side routing`.
-1.  What does HTTP stand for?
-1.  What does CRUD stand for?
-1.  Which HTTP methods can be mapped to the CRUD acronym that we use when interfacing with APIs/Servers.
-1.  Mention three tools we can use to make AJAX requests
+
+In server side routing, a whole new document is loaded when a link is clicked.  With client side routing, the routing is handled by the JavaScript that is loaded on the page.  The data is all imported at the beginning, leading for faster view changes. 
+
+2.  What does HTTP stand for?
+
+HyperText Transfer Protocol
+
+3.  What does CRUD stand for?
+
+CRUD stands for Cread Read Update and Delete
+
+4.  Which HTTP methods can be mapped to the CRUD acronym that we use when interfacing with APIs/Servers.
+
+.get is the read, .post is the create, .put is the update, and .delete is the delete
+
+5.  Mention three tools we can use to make AJAX requests
+
+For AJAX requests, we have several options.  We can use the Axios library, the fetch API as well as with JQUERY.
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,5 +1,29 @@
 import React, { Component } from 'react';
 
+
+const form ={
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems:'center'
+}
+
+const input={
+  border: 'none',
+  borderBottom: '2px solid #00A4D6',
+  width: '300px',
+  margin: '20px'
+}
+
+const button ={
+  width: '200px',
+  padding: '10px',
+  color: '#00A4D6',
+  borderRadius: '15px',
+  border: '1px solid #00A4D6',
+  background: 'white',
+  margin: '20px 0',
+  boxShadow: '0px 8px 15px black'
+}
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
@@ -30,29 +54,35 @@ class SmurfForm extends Component {
 
   render() {
     return (
-      <div className="SmurfForm">
-        <form onSubmit={this.addSmurf}>
+      <div >
+        <form style={form} onSubmit={this.addSmurf}>
           <input
+            style={input}
             onChange={this.handleInputChange}
             placeholder="name"
             value={this.state.name}
+            required='required'
             name="name"
           />
           <input
+            style={input}
             onChange={this.handleInputChange}
             type='number'
             placeholder="age"
             value={this.state.age}
+            required='required'
             name="age"
           />
           <input
+            style={input}
             onChange={this.handleInputChange}
             type='number'
+            required='required'
             placeholder="height"
             value={this.state.height}
             name="height"
           />
-          <button type="submit">Add to the village</button>
+          <button style={button} type="submit">Add to the village</button>
         </form>
       </div>
     );
